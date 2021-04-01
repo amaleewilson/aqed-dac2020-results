29cefc985d14308f0ef47c2700d0938898783d90
Amalee Wilson <amalee@cs.stanford.edu>
2021-04-01 16:51:32 -0700

    fix?

--- Memory_Controller_Case_Study/DoubleBuffer/step0/voutput331.v
+++ Memory_Controller_Case_Study/DoubleBuffer/step0/voutput331.v
@@ -1,7 +1,7 @@
 module aqed_mod_test(
   input  [15:0] data_in, data_out_in,
   input         signal_accvalid, signal_accready, signal_hostvalid,
-  input         full, clk_en, clk, reset, exec_dup,
+  input         clk_en_signal, clk, reset, exec_dup,
   output [15:0] data_out,
   output        qed_done, qed_check);
 
@@ -37,7 +37,7 @@ module aqed_mod_test(
   wire [15:0] _T_10 = 16'h0;	// output331:42:15
   wire [31:0] _T_11 = 32'h0;	// output331:44:15
   wire [31:0] _T_12 = 32'h1;	// output331:45:15
-  wire _T_13 = ~full & ~reset & signal_hostvalid;	// output331:46:11, :48:11, :49:11
+  wire _T_13 =  ~reset & signal_hostvalid;	// output331:46:11, :48:11, :49:11
   wire _T_14 = _T_13 & ~_T_4 & exec_dup;	// output331:47:11, :50:11, :51:11
   assign issue_orig_wire = _T_14;	// output331:52:5
   assign issue_other_wire = _T_13 & ~_T_6 & ~_T_14;	// output331:53:11, :54:11, :55:11, :56:5
@@ -45,8 +45,8 @@ module aqed_mod_test(
     automatic logic _T_15;	// output331:58:13
     automatic logic _T_16;	// output331:59:13
 
-    _T_15 = clk_en & _T_5;	// output331:58:13
-    _T_16 = clk_en & _T_6;	// output331:59:13
+    _T_15 = clk_en_signal & _T_5;	// output331:58:13
+    _T_16 = clk_en_signal & _T_6;	// output331:59:13
     if (reset) begin	// output331:60:7
       orig_issued_reg <= _T_9;	// output331:61:9
       dup_issued_reg <= _T_9;	// output331:62:9
@@ -67,9 +67,9 @@ module aqed_mod_test(
     automatic logic _T_18;	// output331:80:13
     automatic logic _T_19;	// output331:81:13
 
-    _T_17 = clk_en & _T_5;	// output331:79:13
-    _T_18 = clk_en & _T_6;	// output331:80:13
-    _T_19 = clk_en & _T_7;	// output331:81:13
+    _T_17 = clk_en_signal & _T_5;	// output331:79:13
+    _T_18 = clk_en_signal & _T_6;	// output331:80:13
+    _T_19 = clk_en_signal & _T_7;	// output331:81:13
     if (reset) begin	// output331:82:7
       orig_in_reg <= _T_10;	// output331:83:9
       orig_val_reg <= _T_11;	// output331:84:9
@@ -103,16 +103,16 @@ module aqed_mod_test(
       dup_out_reg <= _T_10;	// output331:119:9
       dup_done_reg <= _T_9;	// output331:120:9
     end
-    if (clk_en & signal_accvalid & _T_3 == _T) begin	// output331:111:13, :113:13, :122:7
+    if (clk_en_signal & signal_accvalid &  _T_3 == _T) begin	// output331:111:13, :113:13, :122:7
       orig_out_reg <= data_out_in;	// output331:123:9
       out_count_reg <= _T_3 + _T_12;	// output331:124:15, :125:9
     end
-    if (clk_en & signal_accvalid & _T_3 == _T_1) begin	// output331:112:13, :114:13, :127:7
+    if (clk_en_signal & signal_accvalid &  _T_3 == _T_1) begin	// output331:112:13, :114:13, :127:7
       dup_out_reg <= data_out_in;	// output331:128:9
       out_count_reg <= _T_3 + _T_12;	// output331:129:15, :130:9
       dup_done_reg <= _T_8;	// output331:131:9
     end
-    if (clk_en & _T_7)	// output331:115:13, :133:7
+    if (clk_en_signal & _T_7)	// output331:115:13, :133:7
       out_count_reg <= _T_3 + _T_12;	// output331:134:15, :135:9
   end // always @(posedge)
   assign data_out = the_data_output;	// output331:35:11, :141:5
